6ac145714eadb14d8f76ff7c02791752764d5a1e
Per Lindén <per.linden@epicalgroup.com>
2024-05-08 14:42:29 +0200

    Fix swapped text

--- src/components/Parse.vue
+++ src/components/Parse.vue
@@ -1,13 +1,13 @@
 <template>
 	<div style="tab-size: 2em">
 		<div style="float: right; width: 48%">
-			<h1>SFC</h1>
+			<h1>Transpiled script</h1>
 			<pre style="margin: 0; margin-top: -16px; padding: 0;">
 				<code class="hljs" v-html="transformed" style="height: 75vh;"></code>
 			</pre>
 		</div>
 		<div>
-			<h1>Transpiled script</h1>
+			<h1>SFC</h1>
 			<textarea v-model="scriptText" style="width: 48%; height: 75vh; margin: 0; padding: 8px" />
 			<br clear="all" />
 			<h1>Template</h1>
